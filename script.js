980912aa90fb8cd852794daceed3937e8e1ae466
VitorRamos77 <vitorrreg@gmail.com>
2025-05-15 08:44:15 -0300

    começo do projeto

---
+++ script.js
@@ -0,0 +1,21 @@
+// seleção de elementos
+const display = document.querySelector("#displayInput");
+const botaoIgual = document.querySelector('.igual');
+const botaoPonto = document.querySelector(".ponto");
+const botoesNumeros = document.querySelectorAll(".num");
+const botoesOperadores = document.querySelectorAll(".operador");
+
+// variaveis globais
+let operacaoAtual = ''
+let operador = null
+let valorAnterior = ''
+let calculando = false
+
+//funções
+function insereNumero(){
+    if(calculando){
+
+    }else{
+        
+    }
+}
